63e70b79d0b1eb6bd38b1c072133127cd7014592
Alexey Kutepov <alkutepov@gmail.com>
2021-05-20 20:37:17 +0300

    Initial commit

---
+++ core/src/ru/gb/StarGame.java
@@ -0,0 +1,39 @@
+package ru.gb;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class StarGame extends ApplicationAdapter {
+
+	private SpriteBatch batch;
+	private Texture img;
+	private TextureRegion region;
+
+	private float x;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 10, 10, 200, 200);
+	}
+
+	@Override
+	public void render () {
+		x++;
+		ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
+		batch.begin();
+		batch.draw(img, x, 0);
+		batch.draw(region, 300, 300, 150, 150);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
