4de973d39256319df8b2bb76d79a960ab7e495f9
Vlad Ivanov <ivanovvlad.261996@mail.ru>
2021-05-31 18:48:38 +0300

    Revert

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -50,14 +50,7 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        Gdx.graphics.setTitle("Cords - x: " + screenX + " , y: " + screenY);
-        return super.mouseMoved(screenX, screenY);
-    }
-
-    @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("Touch down in x: " + screenX + " ,y: " + screenY);
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
         v.set(touch.cpy().sub(pos)).setLength(V_LEN);
         return false;
